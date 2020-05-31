df492ac2af7a5fdb1b26a94542ebb4838c66651f
yinlong22 <61413804+yinlong22@users.noreply.github.com>
2020-05-31 16:29:36 +0800

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-App({
-  globalData: {
-      host: 'https://gp-server.hunger-valley.com',  
-      app_id: "wx9d268a65b9620570",
-      app_secret: "fedba72f3f2efd6f70f0e828c3dc9327",
-      t_app_id: "4YAFzJEAVask6a7fKCfuQM13",
-      t_app_secret: "JWANoL5duRuXMfzURLawvu7d"
-  }
- })
